68c5995b46649cfa1058c14cd824baca74c27aff
Ian Cossentino <icossentino@gmail.com>
2020-11-10 16:25:29 -0500

    commit tree 1002c5d1484f01a07fe435f25019205d0177c0b4

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+
+
+def prime?(integer)
+    if integer < 0 || integer == 1
+        return false
+    elsif integer == 2 || integer == 3
+        return true
+    elsif (integer % 2 == 0) || (integer % 3 == 0)
+        return false
+    else
+    	(5..(Integer.sqrt(integer) + 1)).each do |i|
+	    	if ((i - 1) % 6 == 0)
+	        	return false if (integer % i) == 0
+	   		elsif ((i + 1) % 6 == 0)
+	       		return false if (integer % i) == 0
+	    	else
+	        	next
+	        end
+	    end
+    end
+    true
+end
+
+                
+
+
+
